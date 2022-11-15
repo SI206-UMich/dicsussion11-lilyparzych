ff5e4cbe06c3662c54c4f080f055fc0b611ed744
lparzych <lilyparzych@Lilys-MacBook-Pro-27.local>
2022-11-15 16:52:43 -0500

    my best work

--- discussion11.py
+++ discussion11.py
@@ -39,11 +39,16 @@ def create_species_table(cur, conn):
 # TASK 1
 # CREATE TABLE FOR PATIENTS IN DATABASE
 def create_patients_table(cur, conn):
+    cur.execute("DROP TABLE IF EXISTS Patients")
+    cur.execute("CREATE TABLE Patients (pet_id INTEGER PRIMARY KEY, name TEXT, species_id INTEGER, age INTEGER, cuteness INTEGER, aggressiveness INTEGER)")
+    conn.commit()
     pass
 
 
 # ADD FLUFFLE TO THE TABLE
 def add_fluffle(cur, conn):
+    cur.execute("INSERT INTO Patients (pet_id, name, species_id, age, cuteness, aggressiveness) VALUES (?,?,?,?,?,?)",(0, "Fluffle", 0, 3, 90, 100))
+    conn.commit()
     pass
     
 
@@ -59,6 +64,12 @@ def add_pets_from_json(filename, cur, conn):
     json_data = json.loads(file_data)
 
     # THE REST IS UP TO YOU
+    cur.execute("SELECT * FROM Species")
+    species = cur.fetchall()
+    print(species)
+    for i in range(len(json_data)):
+        cur.execute("INSERT INTO Patients (pet_id INTEGER PRIMARY KEY, name TEXT, species_id INTEGER, age INTEGER, cuteness INTEGER, aggressiveness INTEGER) VALUES (?,?,?,?,?,?)",(i,json_data[i]["name"],json_data[i]["species"],json_data[i]["age"],json_data[i]["cuteness"],json_data[i]["aggressiveness"]))
+    conn.commit()
     pass
 
 
